e1599d1a856fbd5d5d5065683c112c4d4f79f210
divine0dsy-cell <divine0dsy@gmail.com>
2026-06-10 16:11:15 +0530

    Create HashsetProg.java

---
+++ HashsetProg.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HashsetProg {
+    public static void main(String args[]){
+        HashSet<String> hs = new HashSet<>();
+        hs.add("Tiger");
+        hs.add("Lion");
+        hs.add("Lion");
+        hs.add("Monkey");
+        hs.add("Cat");
+        System.out.println(hs);
+
+    }
+}
