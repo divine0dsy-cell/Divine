843dc8a7e107219e3fbb1dcfcb628390c98a9a38
divine0dsy-cell <divine0dsy@gmail.com>
2026-06-10 16:12:33 +0530

    Create TreesetProg.java

---
+++ TreesetProg.java
@@ -0,0 +1,13 @@
+import java.util.TreeSet;
+
+public class TreesetProg {
+    public static void main(String args[]){
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("Tiger");
+        ts.add("Lion");
+        ts.add("Lion");
+        ts.add("Monkey");
+        ts.add("Cat");
+        System.out.println(ts);
+    }
+}
