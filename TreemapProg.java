d7db13ba6339e9739ca520af584efe7147785192
divine0dsy-cell <divine0dsy@gmail.com>
2026-06-11 11:18:02 +0530

    Create TreemapProg.java

---
+++ TreemapProg.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class TreemapProg {
+    public static void main(String args[]){
+        Map m1= new TreeMap();
+        m1.put(101, "Aman");
+        m1.put(102, "Raju");
+        m1.put(103, "Rahul");
+        m1.put(104, "Rahul");
+        m1.put(105, "Divya");
+        m1.put(105, "Divya");
+
+        System.out.println(m1);
+    }
+}
